f9138ef1159d4a856ac23fd250f5091220b1e99a
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-04 11:13:21 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,23 @@
+function countWord() {
+		
+
+			var words = document
+				.getElementById("evaluatedText").value;
+		
+			
+			var count = 0;
+		
+			
+			var split = words.split(' ');
+		
+			
+			for (var i = 0; i < split.length; i++) {
+				if (split[i] != "") {
+					count += 1;
+				}
+			}
+		
+			
+			document.getElementById("show")
+				.innerHTML = count;
+		}
